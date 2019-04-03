d35499b2125965b6d3a3071325e731158934d9c6
Eduardo Ferrer <edfmgr@gmail.com>
2019-04-03 02:52:09 -0600

    validation

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-const styles ={
+const styles = {
   container: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -23,34 +23,92 @@ const styles ={
 }
 class Login extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      formErrors: { email: '', password: '' },
+      emailValid: false,
+      passwordValid: false,
+      formValid: false
+    }
+  }
+
+
+
   render() {
     return (
       <div>
-      <NavBar/>
-      <form className={styles.container}autoComplete="off">
-      <TextField
-          required
-          label="Mail"
-          placeholder="Mail"
-          className={styles.textField}
-          margin="normal"
-          variant="outlined"
-        />
-      <TextField
-          label="Password"
-          className={styles.textField}
-          type="password"
-          placeholder="Password"
-          margin="normal"
-          variant="outlined"
+        <NavBar />
+        <form className={styles.container} autoComplete="off">
+          <TextField
+            required
+            label="Mail"
+            placeholder="Mail"
+            className="form-group"
+            name="email"
+            margin="normal"
+            variant="outlined"
+            value={this.state.email}
+            onChange={(event) => this.handleUserInput(event)}
           />
-      <Button variant="outlined" color="primary" className={styles.button}>
-        Login
+          <TextField
+            label="Password"
+            className="form-group"
+            name="password"
+            type="password"
+            placeholder="Password"
+            margin="normal"
+            variant="outlined"
+            value={this.state.password}
+            onChange={(event) => this.handleUserInput(event)}
+          />
+          <Button variant="outlined" color="primary" className={styles.button} disabled={!this.state.formValid}> 
+            Login
       </Button>
-      </form>
-      </div>
+      
+        </form>
+        
+      </div >
     );
   }
+
+  handleUserInput(e) {
+    console.log(e);
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value}, 
+      () => { this.validateField(name, value) });
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+  
+    switch(fieldName) {
+      case 'email':
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+        break;
+      case 'password':
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? '': ' is too short';
+        break;
+      default:
+        break;
+    }
+    this.setState({formErrors: fieldValidationErrors,
+                    emailValid: emailValid,
+                    passwordValid: passwordValid
+                  }, this.validateForm);
+  }
+  
+  validateForm() {
+    this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+  }
+
 }
 
 export default Login;
