eca74f3f8b188ac18d86b6056a86c69f630f2229
HidalgoIvan <A01651425@itesm.mx>
2019-04-03 02:34:57 -0600

    merge conflicts

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './modules/Main.js';
 import InfoBasica from './modules/InfoBasica/InfoBasica.js';
-<<<<<<< HEAD
 import Login from './modules/Login.js';
-=======
 import Tequilas from './modules/Tequilas/Tequilas.js';
-
->>>>>>> vamos a rebasar este pedo
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
           <Route path='/Main' exact component={Main} />
           <Route path='/botella/:tequila' exact component={InfoBasica}/>
-<<<<<<< HEAD
           <Route path='/' exact component={Login} />
-=======
           <Route path='/Tequilas' exact component={Tequilas}/>
-
-
->>>>>>> vamos a rebasar este pedo
       </BrowserRouter>
     );
   }
