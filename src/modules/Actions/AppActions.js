7eafbcdbb2eca93a8f51468871bc267a6175df68
NorbiRm <norbertoreyesmun@gmail.com>
2019-04-03 01:51:00 -0600

    db.json

--- src/modules/Actions/AppActions.js
+++ src/modules/Actions/AppActions.js
@@ -5,7 +5,7 @@ var WheatherAPI = require('../WheatherAPI.js');
 
 module.exports = {
 
-  getWheather: function(city) {
+  getTequila: function(city) {
     AppDispatcher.handleViewAction({
       actionType: Constants.GET_TEQUILA,
       tequila: city
