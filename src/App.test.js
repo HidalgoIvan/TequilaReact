97d2b4c133ae8320e903320e85311db84d2d1869
NorbiRm <norbertoreyesmun@gmail.com>
2019-05-15 17:25:20 -0500

    first tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './modules/Login.js';
 import NavBarPre from './modules/NavBarPre';
+import CardGrid from './modules/CardGrid';
 import InfoBasica from './modules/InfoBasica/InfoBasica.js';
 import Tequilas from './modules/Tequilas/Tequilas.js';
 import Fabricante from './modules/Fabricante/Fabricante.js';
@@ -28,18 +29,23 @@ describe('Login component testing', function() {
   });
 });
 
-describe('Tequilas component testing', function() {
-  it('renders Login correctly', function() {
-    const wrapper = mount(<Tequilas />);
-    console.log("LIST TEQUILAS", wrapper.state());
-    expect(wrapper.state().botellas).to.not.equal({});
+describe('Tequilas cardgrid testing', function() {
+  it('renders Tequila cardgrid correctly', function() {
+    const wrapper = shallow(<Tequilas />);
+    expect(wrapper.matchesElement(<CardGrid />)).to.equal(true);
   });
 });
 
-// describe('Login component button disabled testing', function() {
-//   it('button disabled', function() {
-//     const wrapper = shallow(<Login />);
-//     const className = <NavBarPre />;
-//     expect(wrapper.contains(className)).to.equal(true);
-//   });
-// });
+describe('Tequilas component testing', function() {
+  it('tequila state correctly', function() {
+    const wrapper = shallow(<Tequilas />);
+    expect(wrapper.state().botellas).to.not.equal([]);
+  });
+});
+
+describe('Fabricante testing', function() {
+  it('renders Fabricante state correctly', function() {
+    const wrapper = shallow(<Fabricante match={{params:{ fabricante: "" }}} />);
+    expect(wrapper.state().fabricantes).to.not.equal([]);
+  });
+});
